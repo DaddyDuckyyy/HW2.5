d7b635f0bce1f0090614190f86480f8d9a9760a9
Daria Orlova <da.orlova365@gmail.com>
2024-05-22 09:05:41 +0300

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,19 @@ public class Main {
         } catch (WrongLoginException | WrongPasswordException exception) {
             System.out.println(exception.getMessage());
         }
+        try {
+            Validator.check("java_skypro_go    ","D_1hWiKjjP_9", "D_1hWiKjjP_9");
+        } catch (WrongLoginException | WrongPasswordException exception) {
+            System.out.println(exception.getMessage());
+        }try {
+            Validator.check("java_skypro_go","D_1hWiKjjP_9    ", "D_1hWiKjjP_9");
+        } catch (WrongLoginException | WrongPasswordException exception) {
+            System.out.println(exception.getMessage());
+        }
+        try {
+            Validator.check("java_skypro_go","D_1hWiKjjP_9", "D_1hWiKjjP_9   ");
+        } catch (WrongLoginException | WrongPasswordException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
